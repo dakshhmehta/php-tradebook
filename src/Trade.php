cf32129a830b85da48f832f6d97d7e4d984939eb
Daksh Mehta <dakshhmehta@gmail.com>
2024-08-08 11:59:14 +0530

    Support of dynamic properties in Trade

--- src/Trade.php
+++ src/Trade.php
@@ -4,10 +4,29 @@ namespace Dakshhmehta\PhpTradebook;
 
 class Trade
 {
+    private $properties = []; // Array to store dynamic properties
+
     public function __construct($data)
     {
         foreach ($data as $key => $val) {
-            $this->{$key} = $val;
+            $this->properties[$key] = $val;
         }
+    }
+
+    public function __get($name)
+    {
+        // Check if the property exists in the array
+        if (array_key_exists($name, $this->properties)) {
+            return $this->properties[$name];
+        }
+
+        // Optionally, throw an exception or return null if the property doesn't exist
+        throw new \Exception("Property '$name' does not exist.");
+    }
+
+    public function __set($name, $value)
+    {
+        // Store the property value in the array
+        $this->properties[$name] = $value;
     }
 }
