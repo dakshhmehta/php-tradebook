e920e8e30febba3de8d9c5f5de04c3bfd01f55f0
Daksh Mehta <dakshhmehta@gmail.com>
2024-08-10 10:56:35 +0530

    Fix Trade class

--- src/Trade.php
+++ src/Trade.php
@@ -4,29 +4,21 @@ namespace Dakshhmehta\PhpTradebook;
 
 class Trade
 {
-    private $properties = []; // Array to store dynamic properties
+    public $purchase_secondary_currency;
+    public $purchase;
+    public $original_qty;
+    public $exchange_rate;
+    public $type;
+    public $price;
+    public $qty;
+    public $date;
+    public $symbol;
+    public $id;
 
     public function __construct($data)
     {
         foreach ($data as $key => $val) {
-            $this->properties[$key] = $val;
+            $this->{$key} = $val;
         }
-    }
-
-    public function __get($name)
-    {
-        // Check if the property exists in the array
-        if (array_key_exists($name, $this->properties)) {
-            return $this->properties[$name];
-        }
-
-        // Optionally, throw an exception or return null if the property doesn't exist
-        throw new \Exception("Property '$name' does not exist.");
-    }
-
-    public function __set($name, $value)
-    {
-        // Store the property value in the array
-        $this->properties[$name] = $value;
     }
 }
